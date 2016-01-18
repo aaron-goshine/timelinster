7bc860a4c0dab07f1fd46662657bbd2baac51c66
Goshine <greenlig@gmail.com>
2016-01-18 13:23:05 +0000

    fix html5 canvas padding

--- html5-canvas-app/canvas-app.js
+++ html5-canvas-app/canvas-app.js
@@ -1,38 +1,52 @@
 
 (function (tlfns) {
-
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var data;
   var barHeight = 30;
   var topMargin = 20;
+  var annualPadding = 5;
+  var labelFontSize = 15;
+  var labelLeft = 0;
+
   var draw = function () {
     var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
     gradient.addColorStop('0', '#FFCC00');
     gradient.addColorStop('0.5', 'orange');
     gradient.addColorStop('1.0', '#94BF21');
 
+    ctx.canvas.width = window.innerWidth - (annualPadding * 2);
+    ctx.font = '15px Helvetica';
     ctx.fillStyle = '#ECECEC';
     ctx.fillRect(0, topMargin, canvas.width, barHeight);
-
-    ctx.canvas.width = window.innerWidth;
-    ctx.font = '15px Helvetica';
+    /**
+     * This is where the event sections are created
+     */
     for (var i = 0; i < data.schedule.length; i++) {
       ctx.fillStyle = gradient;
       var eventStyle = tlfns.generateEventStyle(data.schedule[i]);
       ctx.fillRect(canvas.width * Number(eventStyle.n_left),
         topMargin, canvas.width * Number(eventStyle.n_width), barHeight);
     }
+    i = null;
 
+    /**
+     * This is where the event separators are created
+     */
     for (var j = 0; j < data.ticks.length; j++) {
       ctx.fillStyle = 'black';
-      var tickSytyle = tlfns.generateTickStyle(data.ticks[j], 5);
-      ctx.fillRect(canvas.width * Number(tickSytyle.n_left),
-        topMargin, tickSytyle.n_width, barHeight);
+      var tickSytyle = tlfns.generateTickStyle(data.ticks[j]);
+      var left = canvas.width * Number(tickSytyle.n_left);
 
+      if (j > 0) {
+        left = left - tickSytyle.n_width;
+        labelLeft = left - (labelFontSize  * 3);
+      }
+
+      ctx.fillRect(left, topMargin, tickSytyle.n_width, barHeight);
       ctx.fillStyle = gradient;
-      var formattedLabel = tlfns.labelFormat(String(data.ticks[j]));
-      ctx.fillText(formattedLabel, canvas.width * Number(tickSytyle.n_left), 15);
+      ctx.fillText(tlfns.labelFormat(data.ticks[j]), labelLeft, labelFontSize);
+      labelLeft = 0;
     }
   };
 
