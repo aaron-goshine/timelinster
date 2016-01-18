63cd9a466b3d598f0f0c25a1344b58a689cba6cf
Goshine <greenlig@gmail.com>
2016-01-18 15:23:56 +0000

    Fix for d3 extras label and implemented real-time update

--- d3-app-extras/d3js-app-extras.js
+++ d3-app-extras/d3js-app-extras.js
@@ -6,17 +6,21 @@
 
   var draw = function () {
     if(!data) return;
-    d3.select('#main-group').remove();
+
     var dlen = data.schedules.length;
     var lineheight = (viewPortHeight - (padding * dlen)) / dlen;
     var color = d3.scale.category20();
+    d3.select('#main-group').remove();
+
     d3.select('#timeline')
     .style({'width': '100%', 'height': viewPortHeight + padding })
     .append('g')
     .attr({'id': 'main-group', transform: 'translate(0, 30)' })
-    .append('rect').style({'fill': '#ECECEC', 'width': '100%', 'height': viewPortHeight});
+    .append('rect')
+    .style({'fill': '#ECECEC', 'width': '100%', 'height': viewPortHeight});
 
-    d3.map(data.schedules).forEach(function (i, userSchdules) {
+    d3.map(data.schedules)
+    .forEach(function (i, userSchdules) {
       var userColor = color(i);
       var mainGroup = d3.select('#main-group').selectAll('.event-daltas' + i)
       .data(userSchdules.schedule)
@@ -48,46 +52,72 @@
           }, 'class': 'userlabel' + i});
       });
     });
-    var timeline = d3.select('#main-group').selectAll('.ticks').data(data.ticks).enter();
+
+    var timeline = d3.select('#main-group')
+    .selectAll('.ticks')
+    .data(data.ticks)
+    .enter();
+
     var now = new Date();
 
     timeline.append('rect')
-    .attr({'class': 'ticks', 'x': function (tickValue) {
-      return timelineFns.generateTickStyle(tickValue).left;
+    .attr({'class': 'ticks', 'x': function (tickValue, i) {
+      var x = timelineFns.generateTickStyle(tickValue).n_left * 100;
+     return (x > 99) ? (x - 0.1) + '%' : x + '%';
     }})
-    .style({'fill': 'black', 'height': viewPortHeight,
-           'width': function (tickValue) {
+    .style({'fill': 'black', 'height': viewPortHeight, 'width': function (tickValue) {
              return timelineFns.generateTickStyle(tickValue, 1.5).width;
-           }});
+      }
+    });
 
-           timeline.append('text').text(function (tickValue) {
-             return timelineFns.labelFormat(tickValue);
-           }).attr({'y': -gutter, 'class': 'label', 'x': function (tickValue) {
-             return timelineFns.generateTickStyle(tickValue).left;
-           }});
-           timeline.append('rect')
-           .attr({'class': 'now', 'x': function () {
-                         var zerohours = (now.getHours() * 100) + now.getMinutes();
-             return timelineFns.generateTickStyle((now.getHours() * 100) + now.getMinutes()).left;
-           }})
-           .style({'fill': 'red', 'height': viewPortHeight, 'width': 5});
-                  timeline.append('text').text(function (tickValue) {
-                    // timelineFns.labelFormat((now.getHours() * 100) + now.getMinutes());
-                    return ' ';
-                  }).attr({'y': -gutter, 'class': 'label', 'x': function (tickValue) {
-                    return timelineFns.generateTickStyle((now.getHours() * 100) + now.getMinutes()).left;
-                  }});
+// ---
+    timeline.append('text')
+    .text(function (tickValue) {
+      return timelineFns.labelFormat(tickValue);
+    })
+    .attr({'y': -gutter, 'class': 'label', 'x': function (tickValue, i) {
+      var x = timelineFns.generateTickStyle(tickValue).n_left * 100;
+      return (x > 99) ? (x - 4) + '%' : x + '%';
+    }});
+
+    d3.select('#main-group')
+    .append('rect')
+    .attr({'class': 'now', 'x': function () {
+      var zerohours = (now.getHours() * 100) + now.getMinutes();
+      return timelineFns.generateTickStyle((now.getHours() * 100) + now.getMinutes()).left;
+    }})
+    .style({'fill': 'red', 'height': viewPortHeight, 'width': 1})
+    .forEach(function (d) {
+
+    });
+
+    d3.select('#main-group')
+    .append('text')
+      .text(function (tickValue) {
+        return timelineFns.labelFormat((now.getHours() * 100) + now.getMinutes());
+      })
+      .attr({'y': viewPortHeight - (gutter * 3), 'class': 'label', 'x': function (tickValue) {
+        return timelineFns.generateTickStyle((now.getHours() * 100) + now.getMinutes()).left;
+      }});
   };
 
-  window.addEventListener('load', function () {
+  function redrawWithData () {
     d3.json('/multi-schedules.json', function (erro, responseData) {
       data = responseData;
       draw();
     });
+  }
+
+  window.addEventListener('load', function () {
+    redrawWithData();
   });
 
   window.addEventListener('resize', function () {
     draw();
   });
+
+  window.setInterval(function () {
+    redrawWithData();
+  }, 600);
 
 }(timelineFns));
